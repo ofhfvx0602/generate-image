851f67510ec2b3aad74eb96ccea0f360a05e3d09
ofhfvx0602 <rikiya09245555@icloud.com>
2024-04-15 18:35:22 +0900

    done

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
-
 import { Collection } from "@/components/shared/Collection";
 import Header from "@/components/shared/Header";
 import { getUserImages } from "@/lib/actions/image.actions";
@@ -10,11 +9,13 @@ import { getUserById } from "@/lib/actions/user.actions";
 const Profile = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const { userId } = auth();
-
   if (!userId) redirect("/sign-in");
-
   const user = await getUserById(userId);
-  const images = await getUserImages({ page, userId: user._id });
+
+  let images = null;
+  if (user && user._id) {
+    images = await getUserImages({ page, userId: user._id });
+  }
 
   return (
     <>
